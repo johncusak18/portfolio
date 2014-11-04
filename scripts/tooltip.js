45e0be5e1f4dba02c98aae2ef9faa7e084adf68e
Mads Johansen Koch <mkoch@vertic.com>
2014-11-04 16:14:57 -0500

    added frontpage, header and tooltip

---
+++ scripts/tooltip.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+	// Tooltip only Text
+	$('.content .img-wrapper .img-head').hover(function () {
+		// Hover over code
+		var title = $(this).attr('title');
+		$(this).data('tipText', title).removeAttr('title');
+		$('<p class="tooltip"></p>')
+        .text(title)
+        .appendTo('body')
+        .fadeIn('slow');
+	}, function() {
+		// Hover out code
+		$(this).attr('title', $(this).data('tipText'));
+		$(".tooltip").remove();
+	})
+});
