3d1f22c1a3957e84363e16351e84e79ae4b74e4a
Mads Johansen Koch <mkoch@vertic.com>
2014-11-05 13:54:43 -0500

    added bower components

---
+++ scripts/scrollto.js
@@ -0,0 +1,14 @@
+﻿var scrollto = function () {
+
+
+	$(".slide-one .hero-icon-container .hero-icon").click(function () {
+		$('html,body').animate({
+			scrollTop: $(".scrollhere").offset().top
+		},
+			{
+				duration: 1900,
+				easing: 'easeInOutCubic'
+			}
+			);
+	});
+};
