ae6af734cf3ec8f9e5a6fff885d17501402861cf
Mads Johansen Koch <mkoch@vertic.com>
2014-11-06 17:45:47 -0500

    final commit with new styles, cariousel and gallery

--- scripts/scrollto.js
+++ scripts/scrollto.js
@@ -1,14 +1,52 @@
-﻿var scrollto = function () {
+﻿$(document).ready(function () {
 
-
-	$(".slide-one .hero-icon-container .hero-icon").click(function () {
-		$('html,body').animate({
-			scrollTop: $(".scrollhere").offset().top
-		},
-			{
-				duration: 1900,
-				easing: 'easeInOutCubic'
-			}
-			);
+	$("#owl-demo").owlCarousel({
+		autoPlay: false,
+		stopOnHover: false,
+		navigation: false,
+		mouseDrag: true,
+		touchDrag: true,
+		singleItem: true,
+		pagination: false,
+		transitionStyle: "fade"
 	});
-};
+
+});
+$(document).ready(function () {
+	$(".fancybox-button").fancybox({
+		prevEffect: 'none',
+		nextEffect: 'none',
+		closeBtn: false,
+		helpers: {
+			title: { type: 'inside' },
+			buttons: {}
+		}
+	});
+});
+$(document).ready(function () {
+	$(".various").fancybox({
+		maxWidth: 800,
+		maxHeight: 600,
+		fitToView: false,
+		width: '70%',
+		height: '70%',
+		autoSize: false,
+		closeClick: false,
+		openEffect: 'none',
+		closeEffect: 'none'
+	});
+});
+$(document).click(function (e) {
+	if (menuVisible) {
+		menuVisible = false;
+		$(' .mobile-menu-dropdown').hide();
+	}
+});
+
+$('.mobile-menu-button').click(function (e) {
+	menuVisible = true;
+	e.preventDefault();
+	e.stopPropagation();
+	$('.mobile-menu-dropdown').toggle();
+
+});
